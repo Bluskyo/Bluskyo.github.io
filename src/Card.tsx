bec223c53b4b6741be8425a474efea7033aa5c3c
bluskyo <blusky445@gmail.com>
2025-07-15 21:20:04 +0200

    card color gradient implemented.

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,11 +3,31 @@ import './Card.css'
 type ContentProps = {
   title: string
   content: string
+  color1: string
+  color2: string
+  color3?: string
+  stop?: number
+  angle: number
 }
 
 export default function Card(props: ContentProps) {
+
+  let styles = { 
+    background: "red"
+  }
+  
+  if (props.color3) {
+    styles = {
+      background: `linear-gradient(${props.angle}deg, ${props.color1} 0%, ${props.color2} ${props.stop}%, ${props.color3} 100%)` 
+    }
+  } else {
+    styles = {
+      background: `linear-gradient(${props.angle}deg, ${props.color1} 0%, ${props.color2} 100%)` 
+    }
+  }
+
   return (
-    <div className='card'>
+    <div className='card' style={styles}>
       <span className='title'>{props.title}</span>
       <p className='text-content'> 
         {props.content}
--- src/Content.tsx
+++ src/Content.tsx
@@ -6,15 +6,37 @@ function Content() {
     <div className="content-grid">
       <Card 
       title="About" 
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante."
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Mauris vel condimentum enim, eu tempus urna. 
+      Integer varius leo ut fringilla ullamcorper. 
+      Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante.Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper.
+      Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Mauris vel condimentum enim, eu tempus urna. Integer varius leo ut fringilla ullamcorper.
+      Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante. 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. 
+      Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante. 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel condimentum enim, eu tempus urna. 
+      Integer varius leo ut fringilla ullamcorper. Nam tellus quam, vehicula ut rhoncus in, consectetur ac ante."
+      color1="rgba(71, 0, 213, 1)"
+      color2="rgba(37, 0, 111, 1)"
+      angle={180}
       />
       <Card 
       title="Projects"
       content="This project was my favorite!"
+      color1="rgba(213, 0, 110, 1)"
+      color2="rgba(37, 0, 111, 1)"
+      angle={0}
       />
       <Card 
       title="Contact Me"
       content="Please feel free to contact me!"
+      color1="rgba(37, 0, 111, 1)"
+      color2="rgba(85, 41, 172, 1)"
+      color3="rgba(97, 255, 181, 1)"
+      angle={0}
+      stop={41}
       />
     </div>
   )
