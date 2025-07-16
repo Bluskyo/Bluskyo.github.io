b392c47c0e19afb419081de2e20ecb5af343e404
bluskyo <blusky445@gmail.com>
2025-07-16 19:21:51 +0200

    added input fields for contact card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,37 +1,80 @@
+import type { FormEvent } from 'react';
 import './Card.css'
+
+import type { colors, inputField } from './Content';
 
 type ContentProps = {
   title: string
+  secondTitle?: string
   content: string
-  color1: string
-  color2: string
-  color3?: string
-  stop?: number
+  colors: colors[]
   angle: number
+  inputData?: inputField[]
+}
+
+function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault()
+  const formData = new FormData(event.currentTarget)
+
+  const formValues = {
+    email: formData.get('email'),
+    message: formData.get('message')
+  }
+  console.log("Submitted data:", formValues);
 }
 
 export default function Card(props: ContentProps) {
+  const gradientString = props.colors
+  .map(stop => stop.stop !== undefined ? `${stop.color} ${stop.stop}%` : stop.color)
+  .join(', ');
 
-  let styles = { 
-    background: "red"
-  }
-  
-  if (props.color3) {
-    styles = {
-      background: `linear-gradient(${props.angle}deg, ${props.color1} 0%, ${props.color2} ${props.stop}%, ${props.color3} 100%)` 
-    }
-  } else {
-    styles = {
-      background: `linear-gradient(${props.angle}deg, ${props.color1} 0%, ${props.color2} 100%)` 
-    }
-  }
+  const styles: React.CSSProperties = {
+    background: `linear-gradient(${props.angle}deg, ${gradientString})`,
+  };
 
   return (
     <div className='card' style={styles}>
-      <span className='title'>{props.title}</span>
-      <p className='text-content'> 
+      <span className='title'>
+        {props.title}
+      </span>
+
+      {props.secondTitle && <h2>{props.secondTitle}</h2>}
+      
+      <p className='text-content'>
         {props.content}
       </p>
+
+      <form onSubmit={handleSubmit}>
+        {props.inputData?.map((input: inputField, index: number) => {
+          switch (input.type) {
+            case "email":
+              return (
+                <input
+                type={input.type}
+                id={input.name}
+                name={input.name}
+                required={input.required}
+                size={12}
+                placeholder={input.placeholder}
+                />
+              )
+            case "textarea":
+              return (
+                <textarea
+                id={input.name}
+                name={input.name}
+                required={input.required}
+                rows={5}
+                cols={33}
+                placeholder={input.placeholder}
+                />
+              )
+          }
+        })}
+
+        {props.inputData && <button>Submit</button>}
+      </form>
+
     </div>
   )
 }
